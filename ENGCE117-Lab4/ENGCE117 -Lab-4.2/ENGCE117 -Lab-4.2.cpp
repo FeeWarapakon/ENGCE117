9df3ce94fe4dc3b3b06856ec3d1743b96d5285a3
Fee <feenix7248@gmail.com>
2025-12-23 23:09:34 +0700

    Update 23/12/68

--- ENGCE117-Lab4/ENGCE117 -Lab-4.2/ENGCE117 -Lab-4.2.cpp
+++ ENGCE117-Lab4/ENGCE117 -Lab-4.2/ENGCE117 -Lab-4.2.cpp
@@ -1,28 +1,19 @@
 #include <stdio.h>
 
-struct student {
-    char name[20];
-    int age ;
-    char sex ;
-    float gpa;
-} ;
-
-void upgrade(struct student *child) {
-  
-    if ( child->sex == 'M' || child->sex == 'm' ) {
-        child->gpa = child->gpa * 1.10f ; 
-    } else if (child->sex == 'F' || child->sex == 'f') {
-        child->gpa = child->gpa * 1.20f ; 
-    }
+void go( int ***p, int **z ) {
+    *p = z ;  
 }
 
 int main() {
-    struct student aboy ;
-    aboy.sex = 'M' ;
-    aboy.gpa = 3.00f ;
+    int *b = new int ; *b = 10 ;
+    int *c = new int ; *c = 20 ;
+    int **a ;
 
-    upgrade(&aboy) ; 
-    printf("%.2f", aboy.gpa) ; 
+    go( &a, &b ) ;
+    printf( "Input=>%d %p %p %p\n", **a, *a, a, &a ) ;
+    printf( "\n" ) ;
+    go( &a, &c ) ;
+    printf( "Input=>%d %p %p %p\n", **a, *a, a, &a ) ;
 
     return 0 ;
 }//end function
